798fc64c9b35ec87e62bdab2bb8cfa151e934a09
Adomas Nasutavicius <adomas.nasutavicius@knf.stud.vu.lt>
2020-10-18 09:27:01 +0300

    Final version

--- app/src/main/java/com/example/myapplication/Calculations.java
+++ app/src/main/java/com/example/myapplication/Calculations.java
@@ -1,16 +1,16 @@
 package com.example.myapplication;
 
 public class Calculations {
-    public static int calcByWords(){
+
+    public static int calcByWords(String a){
         int x=0;
-
-
+        String[]count = a.split("[ ,.]");
+        x=count.length;
         return x;
     }
-    public static int calcByChars(){
-        int x=0;
-
-
+    public static int calcByChars(String a){
+        int x=a.length();
         return x;
     }
+
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,53 +4,77 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import static android.view.View.FOCUS_DOWN;
 
 public class MainActivity extends AppCompatActivity {
-
     private Spinner optionSpinner;
-   private TextView resultText;
-   private TextView wordResult;
-   private TextView charResult;
-
+    private TextView resultText;
+    private TextView wordResult;
+    private TextView charResult;
+    private TextView enteredText;
+    private TextView warningtext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         this.resultText= findViewById(R.id.calcResult);
         this.resultText.setVisibility(View.GONE);
-        //this.charResult.setVisibility(View.GONE);
-        //this.wordResult.setVisibility(View.GONE);
+        this.charResult = findViewById(R.id.charCountText);
+        this.charResult.setVisibility(View.GONE);
+        this.wordResult = findViewById(R.id.wordCountText);
+        this.wordResult.setVisibility(View.GONE);
         this.optionSpinner = findViewById(R.id.optionSpinner);
+        this.enteredText = findViewById(R.id.textForCalc);
+        this.warningtext = findViewById(R.id.warningText);
+        this.warningtext.setVisibility(View.GONE);
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource( this, R.array.optionsForSpinner, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
         optionSpinner.setAdapter(adapter);
     }
-   /* protected void onStart(){
-        super.onStart();
 
-       this.resultText.setVisibility(View.GONE);
-       this.charResult.setVisibility(View.GONE);
-       this.wordResult.setVisibility(View.GONE);
 
-    }*/
     public void calculation(View view) {
+
         String choice = optionSpinner.getSelectedItem().toString();
-        String words = "words";
-        String functionToString = Integer.toString(Calculations.calcByWords());
+        String words = "By words";
+        String text = this.enteredText.getText().toString();
 
+        if(text.equals("")){
 
-        if (choice.equalsIgnoreCase(words)){
-            //this.resultText.setText(functionToString);
-            //this.resultText.setVisibility(View.VISIBLE);
+            this.warningtext.setVisibility(View.VISIBLE);
+            this.charResult.setVisibility(View.GONE);
+            this.wordResult.setVisibility(View.GONE);
 
-        } else
-            Calculations.calcByChars();
+        } else {
+
+            InputMethodManager manager = (InputMethodManager) getSystemService(this.INPUT_METHOD_SERVICE);
+            manager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+            this.warningtext.setVisibility(View.GONE);
+
+            if (choice.equals(words)) {
+
+                this.charResult.setVisibility(View.GONE);
+                String functionToString = Integer.toString(Calculations.calcByWords(text));
+                this.resultText.setText(functionToString);
+                this.resultText.setVisibility(View.VISIBLE);
+                this.wordResult.setVisibility(View.VISIBLE);
+
+            } else {
+
+                this.wordResult.setVisibility(View.GONE);
+                String functionToString = Integer.toString(Calculations.calcByChars(text));
+                this.resultText.setText(functionToString);
+                this.resultText.setVisibility(View.VISIBLE);
+                this.charResult.setVisibility(View.VISIBLE);
+
+            }
+        }
     }
 }
